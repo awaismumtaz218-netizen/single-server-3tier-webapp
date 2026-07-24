029761e691d5dde391eb45820322b37a923227ba
root <root@ip-172-31-43-181.ec2.internal>
2026-07-24 18:55:23 +0000

    Update backend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'http://100.24.81.9:3000/api/',
   timeout: 10000, // 10 second timeout
   headers: {
     'Content-Type': 'application/json'
